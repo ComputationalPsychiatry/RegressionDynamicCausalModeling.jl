7a113d26c181cbd622e4a53ca7eb1f8129c8b48e
Imre Kertesz <ikertesz@biomed.ee.ethz.ch>
2024-09-24 15:05:21 +0200

    formatting of example

--- examples/Specify_DCM.jl
+++ examples/Specify_DCM.jl
@@ -38,10 +38,10 @@ dcm = LinearDCM(a, c, scans, size(a, 1), U, Y, Ep)
 
 # ## Confounds
 # It is possible to specify confounds. The following example shows how this can be done.
-X0 = zeros(500,3) # specify confound matrix (fist dimension is time, second dimension is number of confounds)
+X0 = zeros(500, 3) # specify confound matrix (fist dimension is time, second dimension is number of confounds)
 conf_names = ["conf1", "conf2", "conf3"]
-conf = Confound(X0,conf_names)
+conf = Confound(X0, conf_names)
 
 # Construct DCM and convert to rDCM structure
-dcm = LinearDCM(a, c, scans, size(a, 1), U, Y, Ep,conf)
+dcm = LinearDCM(a, c, scans, size(a, 1), U, Y, Ep, conf)
 rdcm = RigidRdcm(dcm)
